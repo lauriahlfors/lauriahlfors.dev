7f9468581cc3c8a7240b106fbe4f69e4ee9b5860
Lauri Ahlfors <ahlfroslauri@gmail.com>
2023-11-22 01:26:17 +0200

    feat: add middleware with locale redirecting

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,29 @@
-export default function middleware() {}
+import { NextRequest, NextResponse } from 'next/server';
+import { I18nLocale, i18nConfig } from './i18n';
+import { getMatchingLocale } from './lib/i18n/getMatchingLocale';
+
+export default function middleware(request: NextRequest) {
+  // Internationalization.
+
+  // Loop through available locales in i18n config, set to true when
+  // iterated locale is not found in current request url.
+  const localeNotFound: boolean = i18nConfig.locales.every(
+    (locale: I18nLocale) =>
+      !request.nextUrl.pathname.startsWith(`/${locale}/`) &&
+      request.nextUrl.pathname !== `/${locale}`
+  );
+
+  // Locale not found in request url, redirect to matched locale url.
+  if (localeNotFound) {
+    // Get matching locale for user.
+    const newLocale: I18nLocale = getMatchingLocale(request);
+
+    // Return new url redirect and redirect user to correct locale url.
+    return NextResponse.redirect(
+      new URL(`/${newLocale}/${request.nextUrl.pathname}`, request.url)
+    );
+  }
+}
 
 export const config = {
   // Matcher ignoring /_next/ and /api/
